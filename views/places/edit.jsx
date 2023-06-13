37f52efc2b081319079b4cef9024c0a717a3e7a1
hotredsam <hotredsam@gmail.com>
2023-06-12 20:45:29 -0700

    added an edit stub with more information

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,47 +1,72 @@
 const React = require('react');
-const Def = require('../default');
+const Def = require('../default.jsx');
 
-function EditPage(props) {
-    const place = props.place;
+function edit_form(data) {
+    const { place } = data; // Destructure the 'place' object from 'data'
 
     return (
         <Def>
             <main>
-                <h1>Edit {place.name}</h1>
-                <div className="place-details">
-                    <form method="POST" action={`/places/${props.id}?_method=PUT`}>
-                        <div className="form-group">
-                            <label>Name:</label>
-                            <input type="text" name="name" defaultValue={place.name} />
-                        </div>
-                        <div className="form-group">
-                            <label>Picture URL:</label>
-                            <input type="text" name="pic" defaultValue={place.pic} />
-                        </div>
-                        <div className="form-group">
-                            <label>City:</label>
-                            <input type="text" name="city" defaultValue={place.city} />
-                        </div>
-                        <div className="form-group">
-                            <label>State:</label>
-                            <input type="text" name="state" defaultValue={place.state} />
-                        </div>
-                        <div className="form-group">
-                            <label>Cuisines:</label>
+                <h1>Edit Place</h1>
+                <form method="POST" action={`/places/${place.id}?_method=PUT`}>
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="name">Place Name</label>
                             <input
-                                type="text"
-                                name="cuisines"
-                                defaultValue={place.cuisines.join(", ")}
+                                className="form-control"
+                                id="name"
+                                name="name"
+                                value={place.name} // Pre-populate with place.name
+                                required
                             />
                         </div>
-                        <button type="submit" className="btn btn-primary">
-                            Save
-                        </button>
-                    </form>
-                </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input
+                                className="form-control"
+                                id="pic"
+                                name="pic"
+                                value={place.pic} // Pre-populate with place.pic
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="city">City</label>
+                            <input
+                                className="form-control"
+                                id="city"
+                                name="city"
+                                value={place.city} // Pre-populate with place.city
+                            />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="state">State</label>
+                            <input
+                                className="form-control"
+                                id="state"
+                                name="state"
+                                value={place.state} // Pre-populate with place.state
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input
+                            className="form-control"
+                            id="cuisines"
+                            name="cuisines"
+                            placeholder="Enter cuisines separated by commas"
+                            required
+                            value={place.cuisines.join(', ')} // Pre-populate with place.cuisines
+                        />
+                    </div>
+
+                    <input className="btn btn-primary" type="submit" value="Edit place" />
+                </form>
             </main>
         </Def>
     );
 }
 
-module.exports = EditPage;
+module.exports = edit_form;
