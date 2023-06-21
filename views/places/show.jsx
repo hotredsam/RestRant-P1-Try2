a75f26fd791fd314091d246df6196b10b86c8fed
hotredsam <hotredsam@gmail.com>
2023-06-21 15:39:02 -0700

    made changes to show.jsx allowing for a sentence to form on the show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,13 +9,22 @@ function show(props) {
                 <div className="place-details">
                     <h1>{place.name}</h1>
                     <img className="place-image" src={place.pic} alt={place.name} />
-                    <p><strong>Location:</strong> {place.city}, {place.state}</p>
+                    <h3>
+                        Located in {place.cty}, {place.state}
+                    </h3>
                     <p><strong>Cuisines:</strong> {Array.isArray(place.cuisines) ? place.cuisines.join(", ") : place.cuisines}</p>
                 </div>
                 <div className="place-rating">
                     <h2>Rating</h2>
                     <p>Currently unrated.</p>
                 </div>
+
+                <h3>
+                    {place.showEstablished()}
+                </h3>
+                <h4>
+                    Serving {place.cuisines}
+                </h4>
                 <div className="place-comments">
                     <h2>Comments</h2>
                     <p>No comments yet!</p>
